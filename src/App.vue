ccc70992119848da181567a9b8d3eccaccef4d38
AlexanderPrincipe <principetolentinoa@hotmail.com>
2020-07-15 08:51:13 -0500

    Componente Menu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ComponenteMenu
+    :links=links
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComponenteMenu from './components/ComponenteMenu.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ComponenteMenu
+  },
+  data () {
+    return {
+      links: [
+            {
+                id: '1',
+                title: 'Preguntas más frecuentes',
+                imagen: require('./static/media/icons/GroupDudas.svg'),
+                to: '/preguntas-frecuentes/'
+            },
+            {
+                id: '2',
+                title: 'Planes de Viaje Seguro',
+                imagen: require('./static/media/icons/Group19Copy.svg'),
+                to: '/preguntas-frecuentes/planes-viaje-seguro'
+            },
+            {
+                id: '3',
+                title: 'Cómo comprar Viaje Seguro',
+                imagen: require('./static/media/icons/Group18Copy.svg'),
+                to: '/preguntas-frecuentes/como-comprar-viaje-seguro'
+            }, 
+            {
+                id: '4',
+                title: 'Cobertura de Viaje Seguro',
+                imagen: require('./static/media/icons/Group15Copy.svg'),
+                to: '/preguntas-frecuentes/cobertura-viaje-seguro'
+            },
+            {
+                id: '5',
+                title: 'Modificación de Viaje Seguro',
+                imagen:require('./static/media/icons/Group11Copy.svg'),
+                to: '/preguntas-frecuentes/modificacion-viaje-seguro'
+            },
+            {
+                id: '6',
+                title: 'Utilización del seguro',
+                imagen: require('./static/media/icons/Group2Copy.svg'),
+                to: '/preguntas-frecuentes/utilizacion-viaje-seguro'
+            },
+            {
+                id: '7',
+                title: 'Trámites',
+                imagen: require('./static/media/icons/Group9Copy2.svg'),
+                to: '/preguntas-frecuentes/tramites'
+            },
+            {
+                id: '8',
+                title: 'Sobre COVID-19',
+                imagen: require('./static/media/icons/logocoronavirus.svg'),
+                to: '/preguntas-frecuentes/sobre-covid-19'
+            },
+        ],
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
